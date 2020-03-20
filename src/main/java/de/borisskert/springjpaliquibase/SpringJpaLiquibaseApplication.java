bd766a0d001983fd305dff1d9ed5928769af8c6d
Boris Skert <boris.skert@gmail.com>
2020-03-20 14:19:18 +0100

    Initialize project with Spring Initializr

---
+++ src/main/java/de/borisskert/springjpaliquibase/SpringJpaLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package de.borisskert.springjpaliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/borisskert/springjpaliquibase/SpringJpaLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.borisskert.springjpaliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
